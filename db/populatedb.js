8879a07908b4116f1a1840f48097502a310fb7b7
nainghtweaung <114207394+nainghtweaung@users.noreply.github.com>
2025-07-09 20:19:57 +0630

    Add database population script

---
+++ db/populatedb.js
@@ -0,0 +1,38 @@
+const { Client } = require('pg');
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  fullname VARCHAR(255) NOT NULL,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  admin BOOLEAN DEFAULT FALSE;
+  
+CREATE TABLE IF NOT EXISTS messages (
+  id SERIAL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(500) NOT NULL,
+  text TEXT NOT NULL,
+  date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP, 
+  userid INTEGER REFERENCES users(id) ON DELETE CASCADE;
+  `;
+
+async function main() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+
+    await client.query(SQL);
+    console.log('Database tables created successfully');
+  } catch (error) {
+    console.error('Error populating database:', error);
+  } finally {
+    await client.end();
+    console.log('Database connection closed');
+  }
+}
+
+main();
